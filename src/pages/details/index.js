eea5bf3fd90a5baa56c9a4aa08a5227aa4165174
Marcos Vinicius Lopes <m.vd2@hotmail.com>
2021-10-09 16:35:15 -0300

    AnimeDex- configurações básicas
    instalação de dependências, criação do app e criação de components.

---
+++ src/pages/details/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Details() {
+  return (
+    <section className="section">
+      <h1 className="title is-1">Details</h1>
+    </section>
+  );
+}
+
+export default Details;
---
+++ src/pages/not-found/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <section className="section">
+      <h1 className="title is-1">NotFound</h1>
+    </section>
+  );
+}
+
+export default NotFound;
